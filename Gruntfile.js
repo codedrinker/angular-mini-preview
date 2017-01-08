7be0cd4d321fdcbe0b8e40a622b53d24ef040cbf
codedrinker <gwangchunlei@gmail.com>
2017-01-08 18:29:34 +0800

    init project

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+var grunt = require('grunt');
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        cssmin: {
+            production: {
+                files: {
+                    'dist/css/angular-mini-preview.min.css': ['src/css/angular-mini-preview.css'],
+                    'demo/css/angular-mini-preview.min.css': ['src/css/angular-mini-preview.css']
+                }
+            }
+        },
+
+        uglify: {
+            production: {
+                files: {
+                    'dist/js/angular-mini-preview.min.js': [
+                        'src/js/angular-mini-preview.js'
+                    ],
+                    'demo/js/angular-mini-preview.min.js': [
+                        'src/js/angular-mini-preview.js'
+                    ]
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.loadNpmTasks('grunt-contrib-uglify') ;
+
+    grunt.registerTask('default', ['cssmin', 'uglify']);
+};
